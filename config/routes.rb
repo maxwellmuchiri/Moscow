ec1f3d4c6cc4198954d11d8837e9071a79a176f7
maxwellmuchiri <muchirimaxwel@gmail.com>
2024-02-05 13:17:44 +0300

    api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :liquors
+      resources :vapes
+      resources :extras
+    end
+  end
+
+  # Your other routes go here if needed
 end
